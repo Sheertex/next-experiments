b3cab98d0bdada02ca362b748dafaaaa5799d51c
Simon Walsh <simon.w.walsh@gmail.com>
2020-09-02 17:11:31 -0400

    Extending timeout further

--- src/webpack/tests/webpack-plugin.spec.ts
+++ src/webpack/tests/webpack-plugin.spec.ts
@@ -7,7 +7,7 @@ import webpack from 'webpack';
 import ExtractExperimentsPlugin from '../index';
 import fs from 'fs-extra';
 
-const TIMEOUT = 10000;
+const EXTENDED_TIMEOUT = 15000;
 
 const resultFilePath = path.resolve(__dirname, 'tmp', 'experiments.json');
 
@@ -82,12 +82,16 @@ describe('webpack/webpack-plugin', function () {
         await runWebpack(['empty.jsx']);
       });
 
-      it('returns an empty array', async function () {
-        const result = fs.readJSONSync(resultFilePath);
-        const expectedResult = [];
+      it(
+        'returns an empty array',
+        async function () {
+          const result = fs.readJSONSync(resultFilePath);
+          const expectedResult = [];
 
-        expect(result).toStrictEqual(expectedResult);
-      });
+          expect(result).toStrictEqual(expectedResult);
+        },
+        EXTENDED_TIMEOUT,
+      );
     });
 
     describe('when JSX file includes one experiment', function () {
@@ -95,17 +99,21 @@ describe('webpack/webpack-plugin', function () {
         await runWebpack(['sample.jsx']);
       });
 
-      it('returns an experiments payload with one experiment', async function () {
-        const result = fs.readJSONSync(resultFilePath);
-        const expectedResult = [
-          {
-            experimentsPayload: { experiment: ['variantA', 'variantB'] },
-            pagePathRegex: 'index$',
-          },
-        ];
+      it(
+        'returns an experiments payload with one experiment',
+        async function () {
+          const result = fs.readJSONSync(resultFilePath);
+          const expectedResult = [
+            {
+              experimentsPayload: { experiment: ['variantA', 'variantB'] },
+              pagePathRegex: 'index$',
+            },
+          ];
 
-        expect(result).toStrictEqual(expectedResult);
-      });
+          expect(result).toStrictEqual(expectedResult);
+        },
+        EXTENDED_TIMEOUT,
+      );
     });
 
     it(
@@ -126,48 +134,60 @@ describe('webpack/webpack-plugin', function () {
 
         expect(result).toStrictEqual(expectedResult);
       },
-      TIMEOUT,
+      EXTENDED_TIMEOUT,
     );
 
     describe('it fails', function () {
-      it('when .jsx file has no Experiment import', async function () {
-        expect.assertions(1);
-        await expect(runWebpack(['noImports.jsx'])).rejects.toBeDefined();
-      });
+      it(
+        'when .jsx file has no Experiment import',
+        async function () {
+          expect.assertions(1);
+          await expect(runWebpack(['noImports.jsx'])).rejects.toBeDefined();
+        },
+        EXTENDED_TIMEOUT,
+      );
     });
 
     describe('resolve correct regex', function () {
-      it('for dynamic pages', async function () {
-        await runWebpack(['sample.jsx'], 'pages/products/[handle]');
+      it(
+        'for dynamic pages',
+        async function () {
+          await runWebpack(['sample.jsx'], 'pages/products/[handle]');
 
-        const result = fs.readJSONSync(resultFilePath);
-        const expectedResult = [
-          {
-            experimentsPayload: {
-              experiment: ['variantA', 'variantB'],
+          const result = fs.readJSONSync(resultFilePath);
+          const expectedResult = [
+            {
+              experimentsPayload: {
+                experiment: ['variantA', 'variantB'],
+              },
+              pagePathRegex: '^\\/products\\/[^\\s\\/]+$',
             },
-            pagePathRegex: '^\\/products\\/[^\\s\\/]+$',
-          },
-        ];
+          ];
 
-        expect(result).toStrictEqual(expectedResult);
-      });
+          expect(result).toStrictEqual(expectedResult);
+        },
+        EXTENDED_TIMEOUT,
+      );
 
-      it('for static pages', async function () {
-        await runWebpack(['sample.jsx'], 'pages/products.js');
+      it(
+        'for static pages',
+        async function () {
+          await runWebpack(['sample.jsx'], 'pages/products.js');
 
-        const result = fs.readJSONSync(resultFilePath);
-        const expectedResult = [
-          {
-            experimentsPayload: {
-              experiment: ['variantA', 'variantB'],
+          const result = fs.readJSONSync(resultFilePath);
+          const expectedResult = [
+            {
+              experimentsPayload: {
+                experiment: ['variantA', 'variantB'],
+              },
+              pagePathRegex: '^\\/products$',
             },
-            pagePathRegex: '^\\/products$',
-          },
-        ];
+          ];
 
-        expect(result).toStrictEqual(expectedResult);
-      });
+          expect(result).toStrictEqual(expectedResult);
+        },
+        EXTENDED_TIMEOUT,
+      );
     });
   });
 });
